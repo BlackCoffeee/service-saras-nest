345e0c190fde9e25d3d7f435ccf58daf8977ed35
BlackCoffeee <arief.veriez@gmail.com>
2024-12-27 15:36:37 +0800

    Integrate EdificesModule into AppModule for enhanced functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
-
+import { EdificesModule } from './edifices/edifices.module';
 
 @Module({
-  imports: [CommonModule],
-  controllers: [],
-  providers: [],
+    imports: [CommonModule, EdificesModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
