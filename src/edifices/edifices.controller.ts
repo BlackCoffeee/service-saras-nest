19b94590aa7a1adb519cec6ede14a975f54173e4
BlackCoffeee <arief.veriez@gmail.com>
2024-12-30 13:35:06 +0800

    Enhance EdificesController with authentication and logging
    - Added `BearerAuthGuard` to secure the `getAll` endpoint, ensuring only authenticated users can access it.
    - Introduced `@User()` decorator to retrieve authenticated user information within the `getAll` method.
    - Implemented logging to capture user access details when fetching all edifices, improving traceability and debugging capabilities.

--- src/edifices/edifices.controller.ts
+++ src/edifices/edifices.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Post, HttpCode, HttpStatus, Body } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, HttpStatus, Body, UseGuards, Logger } from '@nestjs/common';
 import { EdificesService } from './edifices.service';
 import { CreateEdificeRequest, EdificeResponse } from '../model/edifices.model';
 import { WebResponse } from '../model/web-response.model';
+import { BearerAuthGuard } from '../auth/guards/bearer-auth.guard';
+import { User } from 'src/auth/decorators/user.decorator';
+import { SSOUser } from 'src/auth/interfaces/sso-user.interface';
 
 @Controller('api/datamaster/edifices')
+@UseGuards(BearerAuthGuard)
 export class EdificesController {
     constructor(private edificeService: EdificesService) {}
 
     @Get()
     @HttpCode(HttpStatus.OK)
-    async getAll(): Promise<WebResponse<EdificeResponse[]>> {
+    async getAll(@User() userInfo: SSOUser): Promise<WebResponse<EdificeResponse[]>> {
         const edifices = await this.edificeService.getAll();
+        Logger.log('user info accessing edifices get all api', userInfo);
         return new WebResponse<EdificeResponse[]>(
             HttpStatus.OK,
             'Edifices fetched successfully',
