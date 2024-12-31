ac167d116b35b9c67b4a1e6b4564955175266f4e
BlackCoffeee <arief.veriez@gmail.com>
2024-12-31 08:43:35 +0800

    Refactor EdificesController to simplify access and logging
    - Removed the `@User()` decorator from the `getAll` method, allowing public access to the endpoint.
    - Updated the controller's route by removing the 'api' prefix for cleaner API structure.
    - Commented out logging of user information to streamline the response for public access.
    - Introduced the `@Public()` decorator to facilitate public endpoint management, enhancing modularity and security.

--- src/edifices/edifices.controller.ts
+++ src/edifices/edifices.controller.ts
@@ -5,17 +5,19 @@ import { WebResponse } from '../model/web-response.model';
 import { BearerAuthGuard } from '../auth/guards/bearer-auth.guard';
 import { User } from 'src/auth/decorators/user.decorator';
 import { SSOUser } from 'src/auth/interfaces/sso-user.interface';
+import { Public } from 'src/auth/decorators/public.decorator';
 
-@Controller('api/datamaster/edifices')
+@Controller('datamaster/edifices')
 @UseGuards(BearerAuthGuard)
 export class EdificesController {
     constructor(private edificeService: EdificesService) {}
 
+    // @Public()
     @Get()
     @HttpCode(HttpStatus.OK)
-    async getAll(@User() userInfo: SSOUser): Promise<WebResponse<EdificeResponse[]>> {
+    async getAll(): Promise<WebResponse<EdificeResponse[]>> {
         const edifices = await this.edificeService.getAll();
-        Logger.log('user info accessing edifices get all api', userInfo);
+        // Logger.log('user info accessing edifices get all api', userInfo);
         return new WebResponse<EdificeResponse[]>(
             HttpStatus.OK,
             'Edifices fetched successfully',
