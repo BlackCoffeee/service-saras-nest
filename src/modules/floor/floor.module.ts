83d75ff7997941fdb408bf747bd686db23bf73a9
BlackCoffeee <arief.veriez@gmail.com>
2025-01-08 23:21:05 +0800

    Enhance FloorModule by adding Winston logging, ValidationService, and PrismaService integration
    - Introduced WinstonModule for logging capabilities within the FloorModule.
    - Added ValidationService to improve data validation processes.
    - Integrated PrismaService for database interactions.
    - Updated provider configuration to include IFloorRepository for better abstraction of data access.

--- src/modules/floor/floor.module.ts
+++ src/modules/floor/floor.module.ts
@@ -1,9 +1,26 @@
 import { Module } from "@nestjs/common";
 import { FloorService } from "./floor.service";
 import { FloorController } from "./floor.controller";
+import { FloorRepository } from "./floor.repository";
+import { ValidationService } from "../../common/validation.service";
+import { WinstonModule } from "nest-winston";
+import { PrismaService } from "src/common/prisma.service";
 
 @Module({
-    providers: [FloorService],
+    imports: [
+        WinstonModule.forRoot({
+            // konfigurasi winston sesuai kebutuhan
+        })
+    ],
+    providers: [
+        FloorService,
+        ValidationService,
+        PrismaService,
+        {
+            provide: 'IFloorRepository',
+            useClass: FloorRepository
+        }
+    ],
     controllers: [FloorController],
     exports: [FloorService],
 })
